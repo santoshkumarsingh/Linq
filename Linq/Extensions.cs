99c09097a8ccaa518f48ed4de663f666ee0a3b7f
Santosh <santosh.ksingh03@gmail.com>
2015-02-13 01:28:38 +0530

    Added some more linq

---
+++ Linq/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public static class Extensions
+    {
+        public static void Dump(this IEnumerable<object> input)
+        {
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void Dump(this string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
